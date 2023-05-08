da66f483166208b2b0dd4b82f9ffffec5727db46
Dmitry <DOskolkov@bk.ru>
2023-05-08 16:44:59 +0300

    hw3

--- HW4 (3)/Program.cs
+++ HW4 (3)/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
