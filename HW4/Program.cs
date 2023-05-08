d577d5830ca2b0542896e94e429196580307b259
Dmitry <DOskolkov@bk.ru>
2023-05-08 16:43:15 +0300

    hw1

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.g
